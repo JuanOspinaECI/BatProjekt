feab0b0402048a1d22f7cd1a8bcf4f7ae03d26cd
JuanOspinaECI <juan.ospina-s@mail.escuelaing.edu.co>
2023-10-16 14:51:25 -0500

    Initial commit

---
+++ BatProjekt.X/main.c
@@ -0,0 +1,13 @@
+/*
+ * File:   main.c
+ * Author: maqui
+ *
+ * Created on October 16, 2023, 2:50 PM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ BatProjekt.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=BatProjekt.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/BatProjekt.X.production.hex
